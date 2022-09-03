778fcc08d4356d29e142d718dc4465dfa9487186
Felipe Ramirez <framirezhites@gmail.com>
2022-09-03 16:09:41 -0400

    Estructura de proyecto

---
+++ libreria/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace libreria
+{
+    public class Class1
+    {
+    }
+}
---
+++ pruebas-unitarias/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace pruebas_unitarias;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
