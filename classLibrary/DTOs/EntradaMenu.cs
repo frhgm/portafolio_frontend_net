bd4be0b8943d1a67ce596957904a3ea5c2dd3c56
Felipe Ramirez <framirezhites@gmail.com>
2022-09-15 00:51:53 -0300

    Nuevo proyecto donde se almacenaran (entre otros) DTOs

---
+++ classLibrary/DTOs/EntradaMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classLibrary
+{
+    public class EntradaMenu
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Ruta { get; set; }
+
+        public EntradaMenu(int Id, string Nombre)
+        {
+            this.Id = Id;
+            this.Nombre = Nombre;
+            this.Ruta = $"{Nombre}.xaml";
+        }
+    }
+}
