2a6f3d6ce6beef20b3052e280bf6244243f4445e
Felipe Ramirez <framirezhites@gmail.com>
2022-09-27 00:08:21 -0300

    Agregue UsuarioSalida por no poder mapear al POST de insert_usuario en la API

--- classLibrary/DTOs/Usuario.cs
+++ classLibrary/DTOs/Usuario.cs
@@ -43,9 +43,12 @@ namespace classLibrary.DTO
         public string NombreRol { get; set; }
 
         [JsonPropertyName("fecha_creacion")]
-        public DateTimeOffset? FechaCreacion { get; set; }
+        public DateTime? FechaCreacion { get; set; }
 
-        public Usuario(string rut, string nombre, string apellidoMaterno, string apellidoPaterno, string email, long telefono, int rolId, string nombreRol, DateTimeOffset? fechaCreacion)
+        [JsonPropertyName("clave")]
+        public string Clave { get; set; }
+
+        public Usuario(string rut, string nombre, string apellidoMaterno, string apellidoPaterno, string email, long telefono, int rolId, string nombreRol, DateTime? fechaCreacion, string clave)
         {
             Rut = rut;
             Nombre = nombre;
@@ -56,6 +59,53 @@ namespace classLibrary.DTO
             RolId = rolId;
             NombreRol = nombreRol;
             FechaCreacion = fechaCreacion;
+            Clave = clave;
+        }
+    }
+    public class UsuarioSalida
+    {
+        [JsonPropertyName("in_rut")]
+        public string Rut { get; set; }
+
+        [JsonPropertyName("in_nombre")]
+        public string Nombre { get; set; }
+
+        [JsonPropertyName("in_apellido_materno")]
+        public string ApellidoMaterno { get; set; }
+
+        [JsonPropertyName("in_apellido_paterno")]
+        public string ApellidoPaterno { get; set; }
+
+        [JsonPropertyName("in_email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("in_telefono")]
+        public long Telefono { get; set; }
+
+        [JsonPropertyName("in_rol")]
+        public int RolId { get; set; }
+
+        [JsonPropertyName("in_nombre_rol")]
+        public string NombreRol { get; set; }
+
+        [JsonPropertyName("fecha_creacion")]
+        public DateTime? FechaCreacion { get; set; }
+
+        [JsonPropertyName("in_clave")]
+        public string Clave { get; set; }
+
+        public UsuarioSalida(string rut, string nombre, string apellidoMaterno, string apellidoPaterno, string email, long telefono, int rolId, string nombreRol, DateTime? fechaCreacion, string clave)
+        {
+            Rut = rut;
+            Nombre = nombre;
+            ApellidoMaterno = apellidoMaterno;
+            ApellidoPaterno = apellidoPaterno;
+            Email = email;
+            Telefono = telefono;
+            RolId = rolId;
+            NombreRol = nombreRol;
+            FechaCreacion = fechaCreacion;
+            Clave = clave;
         }
     }
 }
