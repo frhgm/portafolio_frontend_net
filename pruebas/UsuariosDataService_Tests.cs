8a9cc1f6b93c660be4545c84aa0f33c4cbeb97e5
Felipe Ramirez <framirezhites@gmail.com>
2022-10-16 22:50:12 -0300

    Base para realizar pruebas unitarias

---
+++ pruebas/UsuariosDataService_Tests.cs
@@ -0,0 +1,35 @@
+using app.Data.Implementations;
+using classLibrary.DTO;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace pruebas
+{
+    public class UsuariosDataService_Tests
+    {
+        private UsuariosDataService dataService { get; set; } = null;
+        [SetUp]
+        public void Setup()
+        {
+            dataService = new UsuariosDataService();
+        }
+
+
+        [Test]
+        public async Task TraerUsuarios_Test()
+        {
+            // Assign
+            List<Usuario> listaUsuarios = new();
+            Usuario usuarioUno = new("18392764-7", "Felipe", "Ramirez", "Hites", "framirezhites@maipogrande.cl", 9974303094, 1, "Admin", DateTime.Today, "663401993");
+            listaUsuarios.Add(usuarioUno);
+
+            // Act
+            var usuariosTraidos = await dataService.TraerUsuarios();
+            // Assert 
+            Assert.AreEqual(listaUsuarios.Count > 0, usuariosTraidos.usuarios.Count > 0);
+        } 
+    }
+}
