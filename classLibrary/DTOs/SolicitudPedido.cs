2f781d00896a01ccdc88720f966666a90a5ed107
Felipe Ramirez <framirezhites@gmail.com>
2022-10-16 23:18:57 -0300

    Merge con develop

---
+++ classLibrary/DTOs/SolicitudPedido.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classLibrary.DTOs
+{
+    public class SolicitudPedido
+    {
+        public int id { get; set; }
+        public int usuario_id { get; set; }
+        public DateTime fecha { get; set; }
+        public string direccion { get; set; } = "";
+        public int estado_solicitud { get; set; }
+    }
+}
