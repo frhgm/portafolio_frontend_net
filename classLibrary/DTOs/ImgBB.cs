7c40ab5868c9167a67912c8f735e9fdf6953cd02
Felipe Ramirez <framirezhites@gmail.com>
2022-10-30 14:59:06 -0300

    Data service general para apis externas, y DTO para ImgBB

---
+++ classLibrary/DTOs/ImgBB.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace classLibrary.DTOs
+{
+    public class RequestImgBB
+    {
+        public string key { get; set; }
+        public string image { get; set; }
+        public string name { get; set; }
+
+        public RequestImgBB(string key, string image, string name)
+        {
+            this.key = key;
+            this.image = image;
+            this.name = name;
+        }
+    }
+    public class data
+    {
+        [JsonPropertyName("id")]
+        public string id { get; set; }
+        [JsonPropertyName("title")]
+        public string title { get; set; }
+        [JsonPropertyName("url")]
+        public string url { get; set; }
+    }
+
+    public class Data
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("url_viewer")]
+        public string UrlViewer { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+
+        [JsonPropertyName("display_url")]
+        public string DisplayUrl { get; set; }
+
+        [JsonPropertyName("width")]
+        public string Width { get; set; }
+
+        [JsonPropertyName("height")]
+        public string Height { get; set; }
+
+        [JsonPropertyName("size")]
+        public int Size { get; set; }
+
+        [JsonPropertyName("time")]
+        public string Time { get; set; }
+
+        [JsonPropertyName("expiration")]
+        public string Expiration { get; set; }
+
+        [JsonPropertyName("image")]
+        public Image Image { get; set; }
+
+        [JsonPropertyName("thumb")]
+        public Thumb Thumb { get; set; }
+
+        [JsonPropertyName("medium")]
+        public Medium Medium { get; set; }
+
+        [JsonPropertyName("delete_url")]
+        public string DeleteUrl { get; set; }
+    }
+
+    public class Image
+    {
+        [JsonPropertyName("filename")]
+        public string Filename { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("mime")]
+        public string Mime { get; set; }
+
+        [JsonPropertyName("extension")]
+        public string Extension { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+    }
+
+    public class Medium
+    {
+        [JsonPropertyName("filename")]
+        public string Filename { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("mime")]
+        public string Mime { get; set; }
+
+        [JsonPropertyName("extension")]
+        public string Extension { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+    }
+   public class ResponseImgBB
+    {
+        [JsonPropertyName("data")]
+        public Data Data { get; set; }
+
+        [JsonPropertyName("success")]
+        public bool Success { get; set; }
+
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+    }
+
+    public class Thumb
+    {
+        [JsonPropertyName("filename")]
+        public string Filename { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("mime")]
+        public string Mime { get; set; }
+
+        [JsonPropertyName("extension")]
+        public string Extension { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+    }
+
+}
---
+++ classLibrary/Data Services/ExternalAPIsDataService.cs
@@ -0,0 +1,73 @@
+﻿using classLibrary.DTO;
+using classLibrary.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Security.Policy;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace classLibrary.DataServices
+{
+    public class ExternalAPIsDataService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _imgBBKey = "c5e06383708ec42092d15248d26eaf46";
+        private readonly string _baseImgBB = "https://api.imgbb.com/1/upload";
+;
+        private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+        /// <summary>
+        /// Instancia el data service con la direccion base (para no repetirla en cada llamada)
+        /// </summary>
+        public ExternalAPIsDataService()
+        {
+            _httpClient = new HttpClient();
+            _jsonSerializerOptions = new JsonSerializerOptions();
+        }
+        public async Task<ResponseImgBB> SubirImagen(string base64Imagen, string nombre)
+        {
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _httpClient.Timeout = TimeSpan.FromSeconds(300);
+            var request = new HttpRequestMessage();
+            request.Method = HttpMethod.Post;
+            request.RequestUri = new(_baseImgBB);
+
+            var content = new MultipartFormDataContent();
+
+            content.Add(new StringContent(_imgBBKey), "key");
+            content.Add(new StringContent(base64Imagen), "image");
+            // Ya que el nombre es un parametro opcional lo agregamos al cuerpo solo si es valido
+            if (nombre != String.Empty || nombre != null)
+            {
+                content.Add(new StringContent(nombre), "name");
+            }
+
+            request.Content = content;
+            var header = new ContentDispositionHeaderValue("form-data");
+            request.Content.Headers.ContentDisposition = header;
+
+            var response = await _httpClient.PostAsync(request.RequestUri.ToString(), request.Content);
+            var result = response.Content.ReadAsStringAsync().Result;
+
+
+            //TODO Ver como mapear respuesta
+            var responseAPI = JsonSerializer.Deserialize<ResponseImgBB>(result, _jsonSerializerOptions);
+            if (response.IsSuccessStatusCode)
+            {
+                return responseAPI;
+
+            }
+            return null;
+        }
+    }
+}
