260ba9e189736b61c9cb8aed05674b88ff191241
Felipe Ramirez <framirezhites@gmail.com>
2022-11-01 18:59:19 -0300

    1. Cambio de llamado a menus. 2. DTO general para respuestas API

---
+++ classLibrary/DTOs/Responses.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace classLibrary.DTOs
+{
+    public class ResponseGeneral
+    {
+        [JsonPropertyName("out_glosa")]
+        public string Glosa { get; set; }
+        [JsonPropertyName("out_mensaje_salida")]
+        public string MensajeSalida { get; set; }
+        [JsonPropertyName("out_estado")]
+        public string Estado { get; set; }
+    }
+}
