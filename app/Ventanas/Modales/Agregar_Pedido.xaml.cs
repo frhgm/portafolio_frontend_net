38d0b4c93830818632a120fff8273b43268d0d2e
Felipe Ramirez <framirezhites@gmail.com>
2022-11-14 20:14:44 -0300

    Cambios previos a rebase

--- app/Ventanas/Modales/Agregar_Pedido.xaml.cs
+++ app/Ventanas/Modales/Agregar_Pedido.xaml.cs
@@ -16,8 +16,8 @@ namespace app.Ventanas.Modales
         public Agregar_Pedido()
         {
             InitializeComponent();
-            DetallesPedidoDG.ItemsSource = null;
             UtilidadesLogica.PoblarComboSolicitudes(Add_Solicitud);
+            DetallesPedidoDG.Items.Add(new {Nombre = "Felipe", Calidad = "1000", Rut="18392764-7", Precio=1000, Cantidad=500, Ganancia=500, Total=1500000});
 
         }
 
@@ -26,6 +26,17 @@ namespace app.Ventanas.Modales
             ComboBox cb = (ComboBox)sender;
             solicitudSeleccionada = (SolicitudPedido)cb.SelectedItem;
             Debug.Write(solicitudSeleccionada.Id);
+        }
+
+        private void BuscarProductos_Productor_OnClick(object sender, RoutedEventArgs e)
+        {
+            //TODO Lograr poblar (o no) DataGrid con Productos_Productores coincidentes con Productos_Clientes
+            Producto_Cliente pc
+            // 1. Traer los producto_cliente de la solcitud seleccionada
+            // 2. Llamar a SP_PRODUCTO_PRODUCTOR_PEDIDO con cada producto_cliente.id para saber si sirve
+            // 2.1 Si no, (SP_GET_PRODUCTO_PRODUCTOR_LOOKUP), detener creacion de pedido  
+            // 2.2 Si existen, proceder
+            // 3. Realizar llamada a SP_INSERT_PEDIDO_Y_DETALLE
         }
     }
 }
