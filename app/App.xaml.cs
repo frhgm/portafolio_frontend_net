d70beac18a4d0e26fb24adee43e46db2a001fb46
Felipe Ramirez <framirezhites@gmail.com>
2022-11-20 14:25:59 -0300

    Se instancian una sola vez los distintos dataservices, con tal de evitar HttpClientExhaustion

---
+++ app/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ShowMeTheXAML;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using classLibrary.DataServices;
+
+namespace app
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public SubastaDataService subastaDataService;
+        public PedidosDataService pedidoDataService;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            XamlDisplay.Init();
+            base.OnStartup(e);
+            subastaDataService = new();
+            pedidoDataService = new();
+        }
+    }
+}
